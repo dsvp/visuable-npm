674c88814937d63c8c2afa538e14a3294a0ae002
MUYEOL <hello@dsvp.kr>
2020-03-08 18:43:56 +0900

    Add Background Add-on

--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -1,4 +1,15 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from "@storybook/react";
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.(js|mdx|tsx)$/), module);
+configure(
+  require.context("../src", true, /\.stories\.(js|mdx|tsx)$/),
+  module,
+  addParameters({
+    backgrounds: [
+      { name: "white", value: "#fff", default: true },
+      { name: "lightgray", value: "lightgray" },
+      { name: "gray", value: "gray" },
+      { name: "black", value: "#000" }
+    ]
+  })
+);
